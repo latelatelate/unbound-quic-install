844fc0e1f553982c95e36459d3130412b6a5ebfb
latelatelate <nickmow+github@gmail.com>
2025-01-11 10:00:51 -0800

    Move config to install dir

--- unbound-quic-install.sh
+++ unbound-quic-install.sh
@@ -10,34 +10,41 @@ if [ "$EUID" -ne 0 ]
 fi
 
 rm -f run.log && touch run.log
+
+# TODO: Unzip unbound.tar and move to sbin
 echo "Extracting Unbound"
 tar xzf unbound-quic-1.22.0.tar.gz | tee -a run.log
 cd unbound-quic-1.22.0
 
+# TODO: install extra dependencies
+
+# Generate user for unbound.service
 echo "Generating new user (unbound)"
 sudo groupadd --system unbound | tee -a run.log
 sudo useradd --system --home /var/lib/unbound --shell /usr/sbin/nologin --ingroup unbound unbound | tee -a run.log
 
+# Set permissions
 echo "Creating app directories and configuring ownership"
-sudo mkdir -p /var/lib/unbound /var/log/unbound /etc/unbound | tee -a run.log
+sudo mkdir -p /var/lib/unbound/certs /var/log/unbound /etc/unbound | tee -a run.log
+sudo cp config/unbound.conf /etc/unbond.conf
 sudo chown -R unbound:unbound /var/lib/unbound /var/log/unbound /etc/unbound | tee -a run.log
+sudo chmod -R 755 /var/lib/unbound /var/log/unbound /etc/unbound | tee -a run.log
 
-# TODO: copy config
-
-# TODO: install extra unbound utils before running these?
+# Run unbound utils
 sudo -u unbound /usr/local/sbin/unbound-anchor || echo "Unbound-anchor may have failed to update the root.key used to verify DNSSEC signatures." | tee -a run.log
 sudo -u unbound /usr/local/sbin/unbound-control-setup | tee -a run.log
 
-# TODO: configure systemd
+# Configure systemd
 sudo cp system/unbound.service /etc/systemd/system/unbound.service | tee -a run.log
 sudo systemctl daemon-reload
 
-# TODO: configure apparmor
+# Configure apparmor
 if sudo apparmor_status | grep -q "module is loaded"; then
   sudo cp apparmor.d/usr.sbin.unbound /etc/apparmor.d/usr.sbin.unbound | tee -a run.log
   sudo apparmor_parser -r /etc/apparmor.d/usr.sbin.unbound | tee -a run.log
 fi
 
+# Enable unbound
 systemctl enable unbound | tee -a run.log
 systemctl restart unbound | tee -a run.log
 systemctl status unbound | tee -a run.log
